5ead0e55fc5d3a65fee962ff955330523f8bbf35
mek88 <photomek@gmail.com>
2020-09-23 21:50:22 -0500

    add zero handling to math functions

--- serverless-lab/divide/main.py
+++ serverless-lab/divide/main.py
@@ -7,8 +7,10 @@ def entry(request):
         num1 = int(request_json.get('num1'))
         num2 = int(request_json.get('num2'))
     elif request_args:
-        num1 = request.args.get('num1')
-        num2 = request.args.get('num2')
+        num1 = int(request.args.get('num1'))
+        num2 = int(request.args.get('num2'))
+    if num2 == 0:
+        return "I'm sorry, Dave. I can't let you divide by zero."
     if num1 and num2:
-        return f"{num1} / {num2} = {num1/num2}" if num2 != 0 else "I'm sorry, Dave, I can't let you divide by zero"
-    return "Error. At least one number did not resolve."
+        return f"{num1} / {num2} = {num1/num2}"
+    return f"Error. At least one number did not resolve. Received num1: {num1}, num2: {num2}"
--- serverless-lab/multiply/main.py
+++ serverless-lab/multiply/main.py
@@ -7,8 +7,10 @@ def entry(request):
         num1 = int(request_json.get('num1'))
         num2 = int(request_json.get('num2'))
     elif request_args:
-        num1 = request.args.get('num1')
-        num2 = request.args.get('num2')
+        num1 = int(request.args.get('num1'))
+        num2 = int(request.args.get('num2'))
+    if num1 == 0 or num2 == 0:
+        return f"You multiplied be zero. The anser is 0."
     if num1 and num2:
         return f"{num1} * {num2} = {num1*num2}"
     return "Error. At least one number did not resolve."
